bf90b962c8868780c1fdf65cdd41ad7e477bc49c
nirzaf <mfmfazrin1986@gmail.com>
2022-05-12 14:11:01 +0530

    Add project files.

---
+++ Interfaces/IProduct.cs
@@ -0,0 +1,11 @@
+﻿namespace GraphQLProject.Models;
+
+public interface IProduct
+{
+    Task<List<Product>> GetAllProductsAsync();
+    Task InsertProductAsync(Product product);
+    Task UpdateProductAsync(long id, Product product);
+    Task DeleteProductAsync(long id);
+    Task<Product> GetProductByIdAsync(long id);
+
+}
---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace GraphQLProject.Models
+{
+    public class Product
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
